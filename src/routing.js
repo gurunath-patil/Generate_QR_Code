d0b6a969696457c235a1101ecbf2d517568ba10f
gurunath-patil <gurunathpatil8099@gmail.com>
2024-11-06 14:39:46 +0530

    add vercel config file & change script

--- src/routing.js
+++ src/routing.js
@@ -4,20 +4,32 @@ const QR = require("qrcode")
 
 
 route.post('/qrobject',async (req,res)=>{
-    const {url} = req.body
-    const newQR = await QRCodeGenerate(url);
-    res.json({"url":newQR});
+    try{
+        const {url} = req.body
+        const newQR = await QRCodeGenerate(url);
+        res.json({"url":newQR});
+    }catch(err){
+        res.send(err)
+    }
 })
 
 route.post('/imgelement',async (req,res)=>{
-    const {url} = req.body
-    const newQR = await QRCodeGenerate(url);
-    res.send(`<img src${newQR} alt="QR Code">`)
+    try{
+        const {url} = req.body
+        const newQR = await QRCodeGenerate(url);
+        res.send(`<img src${newQR} alt="QR Code">`)
+    }catch(err){
+        res.send(err)
+    }
 })
 
 async function QRCodeGenerate(URL){
-    const newQRImg = await QR.toDataURL(URL)
-    return newQRImg;
+    try{
+        const newQRImg = await QR.toDataURL(URL)
+        return newQRImg;
+    }catch(err){
+        console.log(err);
+    }
 }
 
 module.exports = route
